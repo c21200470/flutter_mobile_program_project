1ac8660bd46110c32a3798da29443d623100bca8
Hanna <jhn823@gmail.com>
2018-11-22 11:01:31 +0900

    hanna

---
+++ lib/colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+const Rcolor1 = const Color(0xFFEAEAEA);
+const Rcolor2 = const Color(0xFFCACACA);
+const Rcolor3 = const Color(0xFFAAAAAA);
+const Rcolor4 = const Color(0xFF9A9A9A);
+
+const Rcolor5 = const Color(0xFF7A7A7A);
+
+const RcolorGoogle = const Color(0xFFFF0a0a);
+const RcolorGuest = const Color(0xFF000000);
+
+const RcolorProfileBackground = const Color(0xFF000000);
+
+const RcolorError = const Color(0xFFFF3A3A);
+
+const Rcolorfacewhite = const Color(0xFFFFFBFA);
+const Rcolorwhite = Colors.white;
