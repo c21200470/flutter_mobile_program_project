8798d4433283cb6670b1ed4a2f7b6bc466ea6442
Hanna <jhn823@gmail.com>
2018-12-04 14:18:51 +0900

    hanna

--- lib/addproduct.dart
+++ lib/addproduct.dart
@@ -1,12 +1,14 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
-import 'colors.dart';
-import 'package:image_picker/image_picker.dart';
 import 'dart:async';
 import 'dart:io';
+
+import 'package:flutter/material.dart';
+
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:image_picker/image_picker.dart';
 
+import 'colors.dart';
 
 class AddProductPage extends StatefulWidget{
   final FirebaseUser user;
