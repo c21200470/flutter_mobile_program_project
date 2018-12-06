e5e11db879f9245c37fd765899f34f3b60daee55
jeff.sk.yoo <21200470>
2018-12-07 00:46:40 +0900

    Search

--- lib/search.dart
+++ lib/search.dart
@@ -1,162 +1,171 @@
-import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_mobile_program_project/addproduct.dart';
+import 'package:flutter_mobile_program_project/colors.dart';
+import 'package:flutter_mobile_program_project/detail.dart';
+import 'package:flutter_mobile_program_project/post.dart';
 
-import 'post.dart';
-import 'groupinENG.dart';
-
-class SearchPage extends StatefulWidget {
-  static final String route = "search";
-  final String group;
-  SearchPage({ Key key , this.group}) : super(key: key);
+class MySearchPage extends StatefulWidget {
+  //static final String route = "search";
+  final FirebaseUser user;
+  final String groupENG;
+  MySearchPage({ Key key , this.groupENG, this.user}) : super(key: key);
   @override
-  _SearchPageState createState() => new _SearchPageState(group);
-
+  _MySearchPageState createState() => new _MySearchPageState(user, groupENG);
 }
 
-class _SearchPageState extends State<SearchPage>
-{
-  Widget appBarTitle = new Text("Search Sample", style: new TextStyle(color: Colors.white),);
-  Icon actionIcon = new Icon(Icons.search, color: Colors.white,);
-  final key = new GlobalKey<ScaffoldState>();
-  final TextEditingController _searchQuery = new TextEditingController();
-  static String groupENG;
-  final String group;
+class _MySearchPageState extends State<MySearchPage> {
 
-  List<String> _list;
-  bool _IsSearching;
-  String _searchText = "";
+  final String groupENG;
+  final FirebaseUser user;
+  static String Searchword = '';
+  final SearchController = TextEditingController();
 
-  _SearchPageState(this.group) {
-    _searchQuery.addListener(() {
-      if (_searchQuery.text.isEmpty) {
-        setState(() {
-          _IsSearching = false;
-          _searchText = "";
-        });
-      }
-      else {
-        setState(() {
-          _IsSearching = true;
-          _searchText = _searchQuery.text;
-        });
-      }
-    });
+  _MySearchPageState(this.user, this.groupENG);
+
+
+  Widget _buildBody(BuildContext context Orientation orientation){
+  return StreamBuilder<QuerySnapshot>(
+  stream: Firestore.instance.collection('Post/'+groupENG+'/'+groupENG).where('title', isEqualTo: Searchword).snapshots(),
+  builder: (context, snapshot){
+  if (!snapshot.hasData) return LinearProgressIndicator();
+
+  return GridView.count(
+  crossAxisCount: orientation == Orientation.portrait ? 3 : 4,
+  padding: EdgeInsets.all(10.0),
+  childAspectRatio: 6.0 / 9.0,
+  children: _buildGrid(context, snapshot.data.documents),
+  );
+  }
+  );
   }
 
-  @override
-  void initState() {
-    super.initState();
-    _IsSearching = false;
-    init();
-
+  List<Widget> _buildGrid(BuildContext context,
+      List<DocumentSnapshot> snapshot) {
+    //카드리스트
+    return snapshot.map((data) => _buildCards(context, data)).toList();
   }
-  List<String> listBuild(){
-    groupENG = groupinEng(group);
-    StreamBuilder(
-      stream: Firestore.instance.collection('Post/'+groupENG+'/'+groupENG).snapshots(),
-      builder: (context, snapshot){
-        if (!snapshot.hasData) return LinearProgressIndicator();
-        return snapshot.data.documents.map((data) {
-          return Post.fromSnapshot(data).title;
-          }).toList();
+
+  Widget _buildCards(BuildContext context, DocumentSnapshot data) {
+    final post = Post.fromSnapshot(data);
+    final ThemeData theme = Theme.of(context);
+
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) =>
+                DetailPage(user: user, post: post, group: groupENG,),
+          ),
+        );
       },
-    );
-  }
-
-  void init() {
-    _list = List();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      key: key,
-      appBar: buildBar(context),
-      body: new ListView(
-        padding: new EdgeInsets.symmetric(vertical: 8.0),
-        children: _IsSearching ? _buildSearchList() : _buildList(),
+      child: Card(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            AspectRatio(
+              aspectRatio: 11 / 9,
+              child: Image.network(
+                (post.imgurl[0]),
+                fit: BoxFit.cover,
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    SizedBox(height: 10.0),
+                    Text(
+                      post.title,
+                      style: theme.textTheme.display1,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    SizedBox(height: 10.0),
+                    Text(
+                      post.price.toString() + ' 원',
+                      style: theme.textTheme.display2,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
 
-  List<ChildItem> _buildList() {
-    return _list.map((contact) => new ChildItem(contact)).toList();
-  }
-
-  List<ChildItem> _buildSearchList() {
-    if (_searchText.isEmpty) {
-      return _list.map((contact) => new ChildItem(contact))
-          .toList();
-    }
-    else {
-      List<String> _searchList = List();
-      for (int i = 0; i < _list.length; i++) {
-        String  name = _list.elementAt(i);
-        if (name.toLowerCase().contains(_searchText.toLowerCase())) {
-          _searchList.add(name);
-        }
-      }
-      return _searchList.map((contact) => new ChildItem(contact))
-          .toList();
-    }
-  }
-
-  Widget buildBar(BuildContext context) {
-    return new AppBar(
-        centerTitle: true,
-        title: appBarTitle,
-        actions: <Widget>[
-          new IconButton(icon: actionIcon, onPressed: () {
-            setState(() {
-              if (this.actionIcon.icon == Icons.search) {
-                this.actionIcon = new Icon(Icons.close, color: Colors.white,);
-                this.appBarTitle = new TextField(
-                  controller: _searchQuery,
-                  style: new TextStyle(
-                    color: Colors.white,
-
-                  ),
-                  decoration: new InputDecoration(
-                      prefixIcon: new Icon(Icons.search, color: Colors.white),
-                      hintText: "Search...",
-                      hintStyle: new TextStyle(color: Colors.white)
-                  ),
-                );
-                _handleSearchStart();
-              }
-              else {
-                _handleSearchEnd();
-              }
-            });
-          },),
-        ]
-    );
-  }
-
-  void _handleSearchStart() {
-    setState(() {
-      _IsSearching = true;
-    });
-  }
-
-  void _handleSearchEnd() {
-    setState(() {
-      this.actionIcon = new Icon(Icons.search, color: Colors.white,);
-      this.appBarTitle =
-      new Text("Search Sample", style: new TextStyle(color: Colors.white),);
-      _IsSearching = false;
-      _searchQuery.clear();
-    });
-  }
-
-}
-
-class ChildItem extends StatelessWidget {
-  final String name;
-  ChildItem(this.name);
+  Widget appBarTitle = new Text("");
+  Icon actionIcon = new Icon(Icons.search);
   @override
   Widget build(BuildContext context) {
-    return new ListTile(title: new Text(this.name));
-  }
+    return Scaffold(
 
+      appBar: new AppBar(
+        backgroundColor: MainOrangeColor,
+        centerTitle: true,
+        title:
+          TextField(
+            style: new TextStyle(
+              color: Colors.white,
+            ),
+            decoration: new InputDecoration(
+              hintText: "검색어를 입력해주세요.",
+              hintStyle: new TextStyle(color: Colors.white)
+            ),
+            controller: SearchController,
+            //keyboardType: TextInputType.multiline,
+          ),
+        actions: <Widget>[
+          new IconButton(
+              icon: actionIcon,
+              onPressed: (){
+                setState(() {
+                  Searchword=SearchController.text;
+                });
+          })
+        ],
+
+          /*
+          actions: <Widget>[
+            new IconButton(icon: actionIcon,onPressed:(){
+              setState(() {
+                if ( this.actionIcon.icon == Icons.search){
+                  this.actionIcon = new Icon(Icons.close);
+                  this.appBarTitle = new TextField(
+                    style: new TextStyle(
+                      color: Colors.white,
+
+                    ),
+                    decoration: new InputDecoration(
+                        prefixIcon: new Icon(Icons.search,color: Colors.white),
+                        hintText: "Search...",
+                        hintStyle: new TextStyle(color: Colors.white)
+                    ),
+                  );}
+                else {
+                  this.actionIcon = new Icon(Icons.search);
+                  this.appBarTitle = new Text("AppBar Title");
+                }
+
+
+              });
+            } ,),]
+          */
+      ),
+
+      body: OrientationBuilder(
+        builder: (context, orientation) {
+
+          return _buildBody(context,orientation);
+        },
+      ),
+    );
+  }
 }
