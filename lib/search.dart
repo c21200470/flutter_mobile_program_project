9009b801b8c825f1829836011e094d1caf8f3b02
Hanna <jhn823@gmail.com>
2018-12-06 09:48:43 +0900

    hanna

--- lib/search.dart
+++ lib/search.dart
@@ -1,144 +1,174 @@
 import 'package:flutter/material.dart';
-
-import 'package:firebase_storage/firebase_storage.dart';
+import 'package:material_search/material_search.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
-
-
-import 'colors.dart';
 import 'post.dart';
-import 'detail.dart';
+import 'groupinENG.dart';
 
-
-class SearchPage extends StatefulWidget{
+class SearchPage extends StatefulWidget {
   static final String route = "search";
-  final FirebaseUser user;
   final String group;
-
-  const SearchPage({Key key, this.user, this.group}) : super(key: key);
-
+  SearchPage({ Key key , this.group}) : super(key: key);
   @override
-  _SearchPageState createState() => _SearchPageState(user, group);
+  _SearchPageState createState() => new _SearchPageState(group);
+
 }
 
-class _SearchPageState extends State<SearchPage>{
-  final FirebaseUser user;
+class _SearchPageState extends State<SearchPage>
+{
+  Widget appBarTitle = new Text("Search Sample", style: new TextStyle(color: Colors.white),);
+  Icon actionIcon = new Icon(Icons.search, color: Colors.white,);
+  final key = new GlobalKey<ScaffoldState>();
+  final TextEditingController _searchQuery = new TextEditingController();
+  static String groupENG;
   final String group;
 
-  final TextEditingController keyword = new TextEditingController();
+  List<String> _list;
+  bool _IsSearching;
+  String _searchText = "";
 
-  _SearchPageState(this.user, this.group);
+  _SearchPageState(this.group) {
+    _searchQuery.addListener(() {
+      if (_searchQuery.text.isEmpty) {
+        setState(() {
+          _IsSearching = false;
+          _searchText = "";
+        });
+      }
+      else {
+        setState(() {
+          _IsSearching = true;
+          _searchText = _searchQuery.text;
+        });
+      }
+    });
+  }
 
+  @override
+  void initState() {
+    super.initState();
+    _IsSearching = false;
+    init();
 
-  Widget _buildBody(BuildContext context Orientation orientation){
-    return StreamBuilder<QuerySnapshot>(
-      stream: keyword == null
-  ? Firestore.instance.collection('Post').snapshots()
-  : Firestore.instance.collection('Post').where('title', isEqualTo: keyword).snapshots(),
+  }
+  List<String> listBuild(){
+    groupENG = groupinEng(group);
+    StreamBuilder(
+      stream: Firestore.instance.collection('Post/'+groupENG+'/'+groupENG).snapshots(),
       builder: (context, snapshot){
         if (!snapshot.hasData) return LinearProgressIndicator();
-
-        return GridView.count(
-          crossAxisCount: orientation == Orientation.portrait ? 3 : 4,
-          padding: EdgeInsets.all(10.0),
-          childAspectRatio: 6.0 / 9.0,
-          children: _buildGrid(context, snapshot.data.documents),
-        );
-      }
-    );
-  }
-
-  List<Widget> _buildGrid(BuildContext context, List<DocumentSnapshot> snapshot) { //카드리스트
-    return snapshot.map((data) => _buildCards(context, data)).toList();
-  }
-
-  Widget _buildCards(BuildContext context, DocumentSnapshot data){
-    final post = Post.fromSnapshot(data);
-    final ThemeData theme = Theme.of(context);
-
-    return GestureDetector(
-      onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) =>
-                DetailPage(user: user, post: post),
-          ),
-        );
+        return snapshot.data.documents.map((data) {
+          return Post.fromSnapshot(data).title;
+          }).toList();
       },
-      child: Card(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            AspectRatio(
-              aspectRatio: 11 / 9,
-              child: Image.network(
-                (post.imgurl[0]),
-                fit: BoxFit.cover,
-              ),
-            ),
-            Expanded(
-              child: Padding(
-                padding: EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 8.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    SizedBox(height: 10.0),
-                    Text(
-                      post.title,
-                      style: theme.textTheme.title,
-                      maxLines: 1,
-                    ),
-                    SizedBox(height: 10.0),
-                    Text(
-                      post.price.toString() + ' 원',
-                      style: theme.textTheme.body1,
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
     );
+  }
+
+  void init() {
+    _list = List();
+    _list.add("Google");
+    _list.add("IOS");
+    _list.add("Andorid");
+    _list.add("Dart");
+    _list.add("Flutter");
+    _list.add("Python");
+    _list.add("React");
+    _list.add("Xamarin");
+    _list.add("Kotlin");
+    _list.add("Java");
+    _list.add("RxAndroid");
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          color: AddIcon,
-          icon: Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-        elevation: 2.0,
-        backgroundColor: AddProductBackground,
-        title: Container(
-          child: ListTile(
-            title: TextFormField(
-              controller: keyword,
-              decoration: InputDecoration(
-                hintText: '검색',
-              ),
-            ),
-            trailing: IconButton(
-              icon: Icon(Icons.cancel),
-              onPressed: (){keyword.clear();},
-            ),
-          ),
-        ),
-      ),
-      body: OrientationBuilder(
-        builder: (context, orientation){
-          return _buildBody(context, orientation);
-        },
+    return new Scaffold(
+      key: key,
+      appBar: buildBar(context),
+      body: new ListView(
+        padding: new EdgeInsets.symmetric(vertical: 8.0),
+        children: _IsSearching ? _buildSearchList() : _buildList(),
       ),
     );
   }
+
+  List<ChildItem> _buildList() {
+    return _list.map((contact) => new ChildItem(contact)).toList();
+  }
+
+  List<ChildItem> _buildSearchList() {
+    if (_searchText.isEmpty) {
+      return _list.map((contact) => new ChildItem(contact))
+          .toList();
+    }
+    else {
+      List<String> _searchList = List();
+      for (int i = 0; i < _list.length; i++) {
+        String  name = _list.elementAt(i);
+        if (name.toLowerCase().contains(_searchText.toLowerCase())) {
+          _searchList.add(name);
+        }
+      }
+      return _searchList.map((contact) => new ChildItem(contact))
+          .toList();
+    }
+  }
+
+  Widget buildBar(BuildContext context) {
+    return new AppBar(
+        centerTitle: true,
+        title: appBarTitle,
+        actions: <Widget>[
+          new IconButton(icon: actionIcon, onPressed: () {
+            setState(() {
+              if (this.actionIcon.icon == Icons.search) {
+                this.actionIcon = new Icon(Icons.close, color: Colors.white,);
+                this.appBarTitle = new TextField(
+                  controller: _searchQuery,
+                  style: new TextStyle(
+                    color: Colors.white,
+
+                  ),
+                  decoration: new InputDecoration(
+                      prefixIcon: new Icon(Icons.search, color: Colors.white),
+                      hintText: "Search...",
+                      hintStyle: new TextStyle(color: Colors.white)
+                  ),
+                );
+                _handleSearchStart();
+              }
+              else {
+                _handleSearchEnd();
+              }
+            });
+          },),
+        ]
+    );
+  }
+
+  void _handleSearchStart() {
+    setState(() {
+      _IsSearching = true;
+    });
+  }
+
+  void _handleSearchEnd() {
+    setState(() {
+      this.actionIcon = new Icon(Icons.search, color: Colors.white,);
+      this.appBarTitle =
+      new Text("Search Sample", style: new TextStyle(color: Colors.white),);
+      _IsSearching = false;
+      _searchQuery.clear();
+    });
+  }
+
 }
 
+class ChildItem extends StatelessWidget {
+  final String name;
+  ChildItem(this.name);
+  @override
+  Widget build(BuildContext context) {
+    return new ListTile(title: new Text(this.name));
+  }
+
+}
